97aef898883c33170c6a918b796889f30d1214d7
jhemyson sousa <jhemyson_@hotmail.com>
2018-08-16 15:32:00 -0300

    items nav

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,59 @@
 import React from 'react'
-import { Container, NewPlayList } from './styles'
+import { Container, NewPlayList, Nav } from './styles'
 import addPlayListIcon from '../../assets/images/add_playlist.svg'
 
 const Sidebar = () => (
   <Container>
-    <div/>
+    <div>
+      <Nav main>
+        <li>
+          <a href="">Navegar</a>
+        </li>
+        <li>
+          <a href="">Rádio</a>
+        </li>
+      </Nav>
+
+      <Nav>
+        <li>
+          <span>SUA BIBLIOTECA</span>
+        </li>
+        <li>
+          <a href="">Seu Daily Mix</a>
+        </li>
+        <li>
+          <a href="">Tocados recentemente</a>
+        </li>
+        <li>
+          <a href="">Músicas</a>
+        </li>
+        <li>
+          <a href="">Álbums</a>
+        </li>
+        <li>
+          <a href="">Artistas</a>
+        </li>
+        <li>
+          <a href="">Estações</a>
+        </li>
+        <li>
+          <a href="">Vídeos</a>
+        </li>
+        <li>
+          <a href="">Podcasts</a>
+        </li>
+      </Nav>
+
+       <Nav>
+       <li>
+          <span>PLAYLISTS</span>
+        </li>
+        <li>
+          <a href="">Molheres do rock</a>
+        </li>
+
+      </Nav>
+    </div>
 
     <NewPlayList>
       <img src={addPlayListIcon} alt="Adicionar playlist"/>
